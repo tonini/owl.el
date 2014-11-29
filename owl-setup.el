031c2d58472a6b62536e23e45f0354c011ee0643
Samuel Tonini <tonini.samuel@gmail.com>
2014-11-29 13:49:03 +0100

    add example placeholders

--- owl-setup.el
+++ owl-setup.el
@@ -3,5 +3,5 @@
 ;;    (setq owl-documentation-packages '(alchemist))
 ;;    (setq owl-documentation-prefix 'alchemist-)
 
-(setq owl-documentation-packages '(alchemist))
-(setq owl-documentation-prefix 'alchemist-)
+(setq owl-documentation-packages '(your-package))
+(setq owl-documentation-prefix 'your-package-)
